117372855720ba1684272e97c28af6c9743244e2
lesage <noutezalesage@yahoo.com>
2023-12-18 17:37:32 +0100

    update files

--- main.tf
+++ main.tf
@@ -1,1 +1,14 @@
-provider "aws" {}
+provider "aws" {
+  region = var.region
+  # put your acces and secret keys
+  
+}
+
+module "my_vpc" {
+    source                  = "./modules/vpc"
+    vpc_cidr                = "10.0.0.0/16"
+    public_subnets_cidr     = ["10.0.1.0/24", "10.0.2.0/24"]
+    private_subnets_cidr    = ["10.0.3.0/24", "10.0.4.0/24"]
+    azs                     = ["us-west-2a", "us-west-2b"]
+    prefix_name             = var.prefix_name
+}
